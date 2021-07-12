26796c4db19c2ba767c6846d170070a35661add1
bilaz666 <victor.me.marc@gmail.com>
2021-07-12 10:26:36 +0200

    second commit

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -40,19 +40,19 @@ contract DAO is Ownable {
     event TokenBought(address indexed sender, uint256 amount);
     event TokenWithdrew(address indexed sender, uint256 amount);
     event ProposalEnded(uint256 id, string status, uint256 epoch);
-    event Voted(address indexed sender, uint256 amount, uint256 power);
+    event Voted(address indexed sender, uint256 power);
 
     constructor(address token) {
         _token = DAOToken(token);
         _supplyInStock = _token.balanceOf(owner());
-        _rate = 10**3;
+        _rate = 10**15;
     }
 
     receive() external payable {
         require(msg.value != 0, "DAO: Cannot buy 0 Token");
         uint256 tokenAmount = msg.value * _rate;
         require(_supplyInStock >= tokenAmount, "DAO: there is not enought token");
-        _token.transferFrom(owner(), msg.sender, tokenAmount);
+        _token.transferFrom(owner(), address(this), tokenAmount);
         _balances[msg.sender] += tokenAmount;
         emit TokenBought(msg.sender, tokenAmount);
     }
@@ -80,11 +80,7 @@ contract DAO is Ownable {
         return id;
     }
 
-    function vote(
-        uint256 id,
-        Vote vote_,
-        uint256 tokenAmount
-    ) public {
+    function vote(uint256 id, Vote vote_) public {
         require(_balances[msg.sender] >= 1, "DAO: In order to vote you need to have at least 1 Token");
         require(_hasVote[msg.sender][id] == false, "DAO: Already voted");
         require(_proposals[id].status == Status.Running, "DAO: Not a running proposal");
@@ -100,13 +96,12 @@ contract DAO is Ownable {
         } else {
             require(_balances[msg.sender] >= 1, "DAO: Vote costs at least 1 Token");
             if (vote_ == Vote.Yes) {
-                _proposals[id].nbYes += 1 * _power(tokenAmount);
+                _proposals[id].nbYes += _power(_balances[msg.sender]);
             } else {
-                _proposals[id].nbNo += 1 * _power(tokenAmount);
+                _proposals[id].nbNo += _power(_balances[msg.sender]);
             }
-            _balances[msg.sender] -= tokenAmount;
             _hasVote[msg.sender][id] = true;
-            emit Voted(msg.sender, tokenAmount, _power(tokenAmount));
+            emit Voted(msg.sender, _power(_balances[msg.sender]));
         }
     }
 
@@ -118,6 +113,21 @@ contract DAO is Ownable {
         emit TokenWithdrew(msg.sender, totalAmount);
     }
 
+
+    function _power(uint256 amount) private pure returns (uint256) {
+        uint256 nb;
+        if (amount > 0 && amount <= 2) {
+            nb = 1;
+        } else if (amount > 3 && amount <= 6) {
+            nb = 3;
+        } else if (amount > 7 && amount <= 14) {
+            nb = 6;
+        } else {
+            nb = 10;
+        }
+        return nb;
+    }
+
     function proposalById(uint256 id) public view returns (Proposal memory) {
         return _proposals[id];
     }
@@ -125,22 +135,7 @@ contract DAO is Ownable {
     function hasVote(address account, uint256 id) public view returns (bool) {
         return _hasVote[account][id];
     }
-
-    function _power(uint256 tokenAmount) internal pure returns (uint256) {
-        uint256 nb;
-        if (tokenAmount >= 1 && tokenAmount <= 25) {
-            nb = 1;
-        } else if (tokenAmount > 25 && tokenAmount <= 50) {
-            nb = 2;
-        } else if (tokenAmount > 50 && tokenAmount <= 100) {
-            nb = 3;
-        } else {
-            nb = 4;
-        }
-        return nb;
-    }
-
-    function checkPower(uint256 tokenAmount) public view returns (uint256) {
+    function checkPower(uint256 tokenAmount) public pure returns (uint256) {
         return _power(tokenAmount);
     }
 }
